9d6ea8db82621d4d47c47f7ce19414f7077834a6
rindoshingen <zenjukai@gmail.com>
2021-08-04 05:56:49 -0700

    Initial commit

---
+++ src/js/vibe.js
@@ -0,0 +1,13 @@
+// bank some good vibe options
+const vibes = [
+	' ... and you are awesome!',
+	' ... have a wonderful day!',
+	' ... and you have this!',
+	' ... keep at it almost there!'
+];
+
+// choose a random vibe
+var vibe = vibes[Math.floor(Math.random() * Math.floor(vibes.length))];
+
+// display a good vibe
+document.querySelector('.vibe').append(vibe);
