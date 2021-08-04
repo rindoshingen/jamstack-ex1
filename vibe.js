a090362002e276ddf1dcb0a07a03e9f4e8acf168
rindoshingen <zenjukai@gmail.com>
2021-08-04 04:44:43 -0700

    Start 11ty JAMStack

---
+++ vibe.js
@@ -0,0 +1,10 @@
+// bank some good vibe options
+const vibes = [
+	'... and you are awesome!',
+	'... have a wonderful day!',
+	'... and you have this!',
+	'... keep at it almost there!'
+];
+
+// choose a random vibe
+var vibe = vibes[Math.floor(Math.random() * Math.floor(vibes.length))];
